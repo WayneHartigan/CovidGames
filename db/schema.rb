2bd757cef478ac7969d924d2bbb187c49c2e58f8
Wayne Hartigan <hartiganwayne@gmail.com>
2020-03-17 17:28:54 +0000

    Reviews, edit and delete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_155857) do
+ActiveRecord::Schema.define(version: 2020_03_17_165323) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 2020_03_17_155857) do
     t.string "game_img_content_type"
     t.bigint "game_img_file_size"
     t.datetime "game_img_updated_at"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "game_id"
   end
 
   create_table "users", force: :cascade do |t|
